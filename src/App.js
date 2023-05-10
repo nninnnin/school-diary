2bd1e17a938e302bffbc724eb9d6ced8342111b9
nninnnin <nninnnin7@gmail.com>
2023-05-10 21:17:58 +0900

    Initialize

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Homework from './Homework';
+import styled from 'styled-components';
+
+const Array = [];
+
+function App() {
+  return (
+    <>
+      <Homework />
+    </>
+  );
+}
+
+export default App;
